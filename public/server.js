c1a4bdf10e9bffa9c39a122569d840a3a3e67143
archiejohn990-coder <archiejohn990@gmail.com>
2026-04-26 23:59:05 +0800

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,35 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
 const path = require("path");
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 require("dotenv").config();
-=======
-
-require("dotenv").config();
-
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-app.post("/signup", async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.json({ message: "User created successfully" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-const User = mongoose.model("User", userSchema);
->>>>>>> 08ccca71e5a094631b8dc5e022fa5a1cd74c9d6f
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -37,20 +10,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
-=======
-mongoose.connect("mongodb://atlas-sql-69ee08b8a25dbc0359f74e25-gw0snx.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
->>>>>>> 08ccca71e5a094631b8dc5e022fa5a1cd74c9d6f
 
 // serve frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-// home route (optional now)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
